6f3919556e43627b350272d2b74ae755bc33076b
Andreushk <134108089+Andreushk@users.noreply.github.com>
2023-05-30 14:09:20 +0300

    Delete javascript directory

--- javascript/firebase/firebase.js
+++
@@ -1,9 +0,0 @@
-// Firebase конфигурация
-export const firebaseConfig = {
-  apiKey: "AIzaSyB940a0fVW3HEVsFr9LqpC4f9ldw0EoYTE",
-  authDomain: "moviemate-9181.firebaseapp.com",
-  projectId: "moviemate-9181",
-  storageBucket: "moviemate-9181.appspot.com",
-  messagingSenderId: "778465789318",
-  appId: "1:778465789318:web:dca4533b797feaa9ec1807"
-};
